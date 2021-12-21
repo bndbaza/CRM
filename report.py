7ff7496e8dc049616fdcd04b804aa8b41252e252
viktor <bnd-baza@mail.ru>
2021-12-22 01:42:32 +0800

    First

--- report.py
+++ report.py
@@ -1,41 +1,35 @@
+from reportlab.lib import pagesizes
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
-from reportlab.platypus import Table, SimpleDocTemplate
+from reportlab.platypus import Table
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
 
 def Pdf():
-    c = (0,0,255)
-    data = [['1','2','3'],['1','2','3']]
-    pdf = canvas.Canvas('My.pdf',bottomup=1)
-    # pdf = SimpleDocTemplate('My.pdf')
-    pdfmetrics.registerFont(TTFont('rus','Univers.ttf'))
-    pdf.setTitle('Title')
-    pdf.setStrokeColorRGB(255,255,255)
-    pdf.setFillColorRGB(c[0],c[1],c[2])
-    pdf.rect(150,790,50,30,fill=1,stroke=0)
-    pdf.rect(300,790,50,30,fill=1,stroke=0)
-    pdf.setFillColorRGB(255,255,255)
-    pdf.setFont('rus',16)
-    pdf.drawCentredString(175,800,'Пилы')
-    pdf.drawCentredString(325,800,'41')
-    pdf.setFillColorRGB(0,0,0)
-    pdf.drawString(210,800,'№ 1234')
-    pdf.drawString(360,800,'10')
+  pdf = canvas.Canvas('My.pdf', pagesize=A4,bottomup=1)
+  pdf.setTitle('test')
 
-    pdf.save()
+  width, height = A4
 
-    # pdf2 = SimpleDocTemplate('My.pdf')
-    
-    # table = Table(data)
-    # elems = []
-    # elems.append(table)
-    # elems.append(pdf)
+  mainTable = Table([
+    ['QR-code','Pr','PILI','#7123','41','10','P C W M'],
+    ['QR-code','Pr','PILI','#7123','41','10','P C W M'],
+  ],colWidths=[width*0.2,width*0.1,width*0.2,width*0.1,width*0.1,width*0.1,width*0.2],
+    rowHeights=30)
 
-    # pdf2.build(elems)
-    
-    
-    
-    
-    
-    
-    # pdf2.save()
+  mainTable.setStyle([
+    ('GRID',(0,0),(-1,-1),1,'red'),
+    ('BACKGROUND',(2,0),(2,0),colors.blue),
+    ('BACKGROUND',(4,0),(4,0),colors.green),
+    ('TEXTCOLOR',(2,0),(2,0),'white'),
+    ('TEXTCOLOR',(4,0),(4,0),'white'),
+    ('ALIGN',(0,0),(-1,-1),'CENTER'),
+    ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+  ])
+
+  mainTable.wrapOn(pdf,0,780)
+  mainTable.drawOn(pdf,0,780)
+
+  pdf.showPage()
+  pdf.save()
