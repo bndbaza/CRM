b5d1930fb4015287dd78e08509a687677e21285c
viktor <bnd-baza@mail.ru>
2021-12-21 17:00:58 +0800

    first commit

--- report.py
+++ report.py
@@ -1,22 +1,41 @@
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.platypus import Table, SimpleDocTemplate
 
 def Pdf():
-    pdf = canvas.Canvas('My.pdf')
+    c = (0,0,255)
+    data = [['1','2','3'],['1','2','3']]
+    pdf = canvas.Canvas('My.pdf',bottomup=1)
+    # pdf = SimpleDocTemplate('My.pdf')
+    pdfmetrics.registerFont(TTFont('rus','Univers.ttf'))
     pdf.setTitle('Title')
-    pdf.setStrokeColorRGB(0,0,0)
-    pdf.setFillColorRGB(0,0,255)
-    pdf.rect(300,790,50,30,fill=1)
+    pdf.setStrokeColorRGB(255,255,255)
+    pdf.setFillColorRGB(c[0],c[1],c[2])
+    pdf.rect(150,790,50,30,fill=1,stroke=0)
+    pdf.rect(300,790,50,30,fill=1,stroke=0)
+    pdf.setFillColorRGB(255,255,255)
+    pdf.setFont('rus',16)
+    pdf.drawCentredString(175,800,'Пилы')
+    pdf.drawCentredString(325,800,'41')
     pdf.setFillColorRGB(0,0,0)
-    pdf.drawString(300,800,'1234567890')
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    pdf.drawString(210,800,'№ 1234')
+    pdf.drawString(360,800,'10')
+
     pdf.save()
+
+    # pdf2 = SimpleDocTemplate('My.pdf')
+    
+    # table = Table(data)
+    # elems = []
+    # elems.append(table)
+    # elems.append(pdf)
+
+    # pdf2.build(elems)
+    
+    
+    
+    
+    
+    
+    # pdf2.save()
